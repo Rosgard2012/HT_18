1f2eca5cbae93cd9372d57d5687768832d95fa2b
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2024-01-03 20:56:24 +0200

    g

--- django_products_scraper/main/views.py
+++ django_products_scraper/main/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 
 def index(request):
-    return HttpResponse('Home page.')
+    return render(request, 'index.html')
 
 
 def about(request):

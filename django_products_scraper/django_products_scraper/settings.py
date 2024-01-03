18dfb256fcd432889b825bc189b06f7453b3392b
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2024-01-03 19:59:54 +0200

    4

--- django_products_scraper/django_products_scraper/settings.py
+++ django_products_scraper/django_products_scraper/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scraper_service.apps.ScraperServiceConfig',
+    'scraper_service'
 ]
 
 MIDDLEWARE = [
@@ -77,6 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'timeout': 80,
     }
 }
 
